d492d07b7cf9a340e783987712590aac7a6589aa
idariyadhara <143485315+idariyadhara@users.noreply.github.com>
2023-08-29 12:59:18 +0530

    Add files via upload

---
+++ Script_13.php
@@ -0,0 +1,7 @@
+<?php
+//13.Write a PHP program that uses concatenation to display an HTML anchor (<a>) element with the
+//text "Click here" and a link to "https://www.example.com".
+
+echo "<a href=https://www.example.com/>Click Here</a>";
+
+?>
