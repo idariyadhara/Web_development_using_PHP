2796f404dbb77db37d31b956aa52a8ea4b671120
idariyadhara <143485315+idariyadhara@users.noreply.github.com>
2023-08-29 12:58:59 +0530

    Add files via upload

---
+++ Script_12.php
@@ -0,0 +1,9 @@
+<?php
+//12.Develop a PHP program that uses echo to display an unordered list (<ul>) with three list items (<li>)
+//containing different colors.
+echo "<ul>";
+echo "<li><font color=red>Red</font></li>";
+echo "<li><font color=blue>Blue</font></li>";
+echo "<li><font color=green>yellow</font></li>";	
+"</ul>";
+?>
