3f69e1bed47ec63458dcd7714785c6eb78568df5
idariyadhara <143485315+idariyadhara@users.noreply.github.com>
2023-08-29 12:28:42 +0530

    Add files via upload

---
+++ Script_8.php
@@ -0,0 +1,8 @@
+<?php
+//8. Develop a PHP program that assigns a name to the variable $name and uses it to display a dynamic
+//greeting message like "Hello, John!".
+
+$name="John";
+
+echo "Hello , $name !";
+?>
