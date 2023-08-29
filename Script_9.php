81427278380cebd3b616199dce8969de078b1763
idariyadhara <143485315+idariyadhara@users.noreply.github.com>
2023-08-29 12:29:11 +0530

    Add files via upload

---
+++ Script_9.php
@@ -0,0 +1,10 @@
+<?php
+//9. Write a PHP program that assigns a string to the variable $text and uses the strlen() function to
+//display its length along with the original string.
+
+$text="Idariya Dhara";
+
+echo "Your String is: $text";
+echo "<br>";
+echo strlen($text);
+?>
