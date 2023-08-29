b86b27e41240316efc40365afb1f242bddd72795
idariyadhara <143485315+idariyadhara@users.noreply.github.com>
2023-08-29 12:27:00 +0530

    Add files via upload

---
+++ Script_2.php
@@ -0,0 +1,12 @@
+<?php
+
+//2. Create a PHP program that uses concatenation to display the following message:
+//i. My name is John, and I am a PHP developer.
+
+$a="My Name is John And ";
+$b="I am php developer";
+
+$c=$a.$b;
+
+echo "$c";
+?>
