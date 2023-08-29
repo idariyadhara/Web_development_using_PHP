4d7e159cc9683555f471482e7ecf0074e5a579f9
idariyadhara <143485315+idariyadhara@users.noreply.github.com>
2023-08-29 12:28:04 +0530

    Add files via upload

---
+++ Script_6.php
@@ -0,0 +1,11 @@
+<?php
+//6. Develop a PHP program that assigns an initial value to the variable $x, then reassigns it to a new
+//value, and finally echoes the value.
+
+$x=10;
+$y=$x;
+
+echo "$y";
+
+?>
+
