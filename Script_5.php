de9d8c49d41ffcb09920953e78bbc597bb4fc33a
idariyadhara <143485315+idariyadhara@users.noreply.github.com>
2023-08-29 12:27:43 +0530

    Add files via upload

---
+++ Script_5.php
@@ -0,0 +1,10 @@
+<?php
+//5. Create a PHP program that assigns a number to the variable $num and a string to the variable $text.
+//Use concatenation to display a message like "The number is: 42, and the text is: Hello!".
+
+$num=42;
+$text="Hello";
+
+echo "The Number is :$num and the text is: $text"
+
+?>
