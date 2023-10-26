8996998caa90eeaa1a19d09b85fc3c1abfc825d5
idariyadhara <143485315+idariyadhara@users.noreply.github.com>
2023-10-26 13:57:01 +0530

    Create Script_26.php

---
+++ Task-3/Script_26.php
@@ -0,0 +1,14 @@
+<?php 
+
+// Script_26 Write a PHP script that generates a random number between a specified range using the rand() function.
+
+
+$random=array(1,2,3,4,5);
+print_r(array_rand($random));
+
+/*
+OUTPUT:
+3
+*/
+
+?>
